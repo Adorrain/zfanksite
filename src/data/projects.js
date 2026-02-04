473b079bab8d726be752edd2eb98620b89472dc6
Adorrain <lh0115zyf@163.com>
2026-02-04 21:54:08 +0800

    change private project

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,16 +1,16 @@
 export const projects = [
-  {
-    id: "NetOps",
-    title: "NetOps 网络运维数字孪生系统",
-    description: "基于 NetOps 理念的网络运维数字孪生系统，通过 YAML 脚本定义网络设备参数，使用 Three.js 进行 3D 可视化展示，实现网络拓扑的数字孪生仿真",
-    content: `
-# 内容还在建设中...
+//   {
+//     id: "NetOps",
+//     title: "NetOps 网络运维数字孪生系统",
+//     description: "基于 NetOps 理念的网络运维数字孪生系统，通过 YAML 脚本定义网络设备参数，使用 Three.js 进行 3D 可视化展示，实现网络拓扑的数字孪生仿真",
+//     content: `
+// # 内容还在建设中...
 
-![封面](/assets/image.png)
-    `,
-    tags: ["React", "three", "netops", "python"],
-    github: "https://github.com/Adorrain/NetOps-Twin-Operation",
-    demo: "https://zfanksite.com",
-    color: "from-blue-500 to-cyan-500"
-  }
+// ![封面](/assets/image.png)
+//     `,
+//     tags: ["React", "three", "netops", "python"],
+//     github: "https://github.com/Adorrain/NetOps-Twin-Operation",
+//     demo: "https://zfanksite.com",
+//     color: "from-blue-500 to-cyan-500"
+//   }
 ];
